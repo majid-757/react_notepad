82a49218b50470db0879df2d0621bd0b6d178019
unknown <majid.asadollahi95@gmail.com>
2023-10-20 20:07:26 +0330

    first commit

---
+++ src/components/Post/Post.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+
+import './Post.css'
+
+
+
+const post = (props) => (
+    <article className='post'>
+        <h1>{props.title}</h1>
+        <div>
+            <div className='author'>Author</div>
+        </div>
+    </article>
+
+)
+
+
+export default post
+
+
---
+++ src/containers/Blog/Blog.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import axios from 'axios';
+
+import Post from '../../components/Post/Post'
+import FullPost from '../../components/FullPost/FullPost'
+import NewPost from '../../components/NewPost/NewPost'
+
+
+import './Blog.css'
+
+
+class Blog extends React.Component {
+
+    state = {
+        posts: [],
+
+    }
+
+    componentDidMount() {
+        axios.get('https://jsonplaceholder.typicode.com/posts').then((response) => {
+            this.setState({ posts: response.data })
+        })
+    }
+
+    render() {
+
+        const posts = this.state.posts.map((item) => {
+            return <Post key={item.id} title={item.title}/>
+        })
+
+        return (
+            <div>
+                <section className='posts'>{posts}</section>
+                <section>
+                    <FullPost />
+                </section>
+                <section>
+                    <NewPost />
+                </section>
+            </div>
+        )
+    }
+}
+
+
+
+export default Blog
+
+
+
